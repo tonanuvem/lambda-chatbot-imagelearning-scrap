11a0358194c1b4efaa3dd7aa90d112eb76cda06e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-07 12:22:37 -0300

    Update install_sls.sh

--- functions/install_sls.sh
+++ functions/install_sls.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
+echo "Instalando as dependencias"
 npm install -g serverless
 serverless --version
-chmod +x scrap/config_sls.sh
-./scrap/config_sls.sh
+chmod +x scrap/sls_config.sh
+#CONFIGURAR
+echo "Configurando as dependencias"
+cd scrap
+sls_config.sh
+cd ..
+#INSTALAR
+echo "Deseja instalar (y/N)": 
+read INSTALAR
+if ($INSTALAR=="y");
+then
+  cd scrap
+  sls_config.sh
+  cd ..
+else
+  echo "Escolhido não instalar"
+fi
