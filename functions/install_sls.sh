d46d758370cd3d87453dba2c7f62332fd7f45678
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-07 18:23:29 -0300

    Update install_sls.sh

--- functions/install_sls.sh
+++ functions/install_sls.sh
@@ -10,7 +10,10 @@ chmod +x sls_config.sh
 ./sls_config.sh
 chmod +x sls_exec.sh
 cd ..
+# CONFIGURAR BUCKET e IAM
 # CONFIGURAR IAM
+echo "Criando Bucket chamado megahack"
+aws s3api create-bucket --bucket megahack --region us-east-1 --create-bucket-configuration LocationConstraint=us-east-1
 echo "Configurando as AWS IAM. Será preciso ajustar o arquivo serverless.yaml com ARN:"
 aws iam create-role --role-name s3lambda --assume-role-policy-document file://s3lambda.json
 echo "Após ajustar o ARN no arquivo serverless.yml de cada function, digite qualquer tecla para continuar."
