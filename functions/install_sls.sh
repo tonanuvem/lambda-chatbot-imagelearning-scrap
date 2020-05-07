5f9df897f66c40bc33abbcd8726a5019c381d663
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-07 17:20:58 -0300

    Update install_sls.sh

--- functions/install_sls.sh
+++ functions/install_sls.sh
@@ -11,9 +11,9 @@ chmod +x sls_config.sh
 chmod +x sls_exec.sh
 cd ..
 #INSTALAR
-echo "Deseja instalar (y/N)": 
+echo -p "Deseja instalar (y/N)": 
 read INSTALAR
-if ("$INSTALAR"=="y");
+if [ $INSTALAR == y ];
 then
   cd scrap
   ./sls_exec.sh
