582fa1ec3db698fd789e1a95e8259c475cb9172f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-07 17:39:43 -0300

    Update install_sls.sh

--- functions/install_sls.sh
+++ functions/install_sls.sh
@@ -10,6 +10,11 @@ chmod +x sls_config.sh
 ./sls_config.sh
 chmod +x sls_exec.sh
 cd ..
+# CONFIGURAR IAM
+echo "Configurando as AWS IAM. Será preciso ajustar o arquivo serverless.yaml com ARN:"
+aws iam create-role --role-name Test-Role --assume-role-policy-document 
+echo "Após ajustar o ARN no arquivo serverless.yml, digite qualquer tecla para continuar."
+read CONTINUAR
 #INSTALAR
 echo "Deseja instalar (y/N) : " 
 read INSTALAR
